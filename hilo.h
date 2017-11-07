8e50695e0e27a6c664c7ec0f45ad6ed846adb5e2
ricardoSE11 <ricardoshumestrada@gmail.com>
2017-11-07 14:42:14 -0600

    Clase de hilo El emit envía un aleatorio entre 1 y 10000000

--- hilo.h
+++ hilo.h
@@ -9,22 +9,7 @@ class Hilo : public QThread
     Q_OBJECT
 public:
     Hilo(int segundos , QObject *parent = nullptr);
-    void run(){    int aleatorio = 0;
-                   srand (time(NULL));
-
-                   if (pausa)
-                   {
-                       this->sleep(1);
-                   }
-
-                   while (!pausa)
-                   {
-                       aleatorio = rand() % 10000000;
-                       emit aleatorioDeSalvacion(aleatorio);
-
-                       this->sleep(misSegundos);
-                   }
-              }
+    void run();
     bool pausa;
     int misSegundos;
 
